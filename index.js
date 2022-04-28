11394ade17b1461ad2fe2bd4bc6cce6df1b49d55
foadtkf <fakhrulif@gmail.com>
2022-04-28 09:57:39 +0600

    connection with database built successfully

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port =process.env.PORT || 5000
+const app=express()
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+// paste the "full driver code example" from mongodb connect to cluster
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bga79.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+try{
+await client.connect()
+const productCollection = client.db('emajohn').collection('product')
+
+app.get('/product',async(req,res)=>{
+    const query ={}
+    const cursor =productCollection.find(query)
+    const products =await cursor.toArray()
+    res.send(products)
+})
+}
+finally{
+
+}
+}
+run().catch(console.dir)
+
+app.get('/', (req,res)=>{
+    res.send('ema-john-server is running and waiting for requests')
+})
+app.listen(port,()=>{
+    console.log('it is running on ',port)
+})
