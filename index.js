02921a889edd09bde93a33ab449f5f615861d811
foadtkf <fakhrulif@gmail.com>
2022-05-01 13:23:44 +0600

    pagination added in backend

--- index.js
+++ index.js
@@ -1,39 +1,71 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const port =process.env.PORT || 5000
-const app=express()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 5000;
+const app = express();
 
 //middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // paste the "full driver code example" from mongodb connect to cluster
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bga79.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
-try{
-await client.connect()
-const productCollection = client.db('emajohn').collection('product')
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("emajohn").collection("product");
+    console.log("Connected");
+    app.get("/products", async (req, res) => {
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      console.log("query", req.query);
+      const query = {};
+      const cursor = productCollection.find(query);
+      let products;
+      if (page || size) {
+        products = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
+      } else {
+        products = await cursor.toArray();
+      }
+      res.send(products);
+      // console.log(products)
+    });
 
-app.get('/product',async(req,res)=>{
-    const query ={}
-    const cursor =productCollection.find(query)
-    const products =await cursor.toArray()
-    res.send(products)
-})
-}
-finally{
+    app.get("/productcount", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const count = await productCollection.countDocuments();
+      // res.json(count)
+      res.json({ count });
+    });
 
+    app.post("/productByKeys", async (req, res) => {
+      const keys = req.body;
+      const ids = keys.map((id) => ObjectId(id));
+      const query = { _id: { $in: ids } };
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      console.log(keys);
+      res.send(products);
+    });
+  } finally {
+  }
 }
-}
-run().catch(console.dir)
+run().catch(console.dir);
 
-app.get('/', (req,res)=>{
-    res.send('ema-john-server is running and waiting for requests')
-})
-app.listen(port,()=>{
-    console.log('it is running on ',port)
-})
+app.get("/", (req, res) => {
+  res.send("ema-john-server is running and waiting for requests");
+});
+app.listen(port, () => {
+  console.log("it is running on ", port);
+});
